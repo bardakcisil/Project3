0bb2006bf0830f09b9cabe9aa6ffc704caa421eb
Işıl BARDAKCI <bardakci.isil@hotmail.com>
2022-07-28 11:04:11 +0300

    version1

--- ConsoleAppUI/Program.cs
+++ ConsoleAppUI/Program.cs
@@ -8,22 +8,28 @@ namespace ConsoleAppUI
     {
         static void Main(string[] args)
         {
-            // ProductTest();
+            ProductTest();
+            //CategoryTest();
+
+        }
+
+        private static void CategoryTest()
+        {
             CategoryManager categoryManager = new CategoryManager(new EFCategoryDal());
             foreach (var category in categoryManager.GetAll())
             {
                 Console.WriteLine(category.CategoryName);
             }
-
         }
 
         private static void ProductTest()
         {
             ProductManager productManager = new ProductManager(new EFProductDal());
-            foreach (var product in productManager.GetAllByUnitPrice(4, 100))
+            foreach (var product in productManager.GetProductDetails())
             {
-                Console.WriteLine(product.ProductName);
+                Console.WriteLine(product.ProductName+ "/"+ product.CategoryName);
             }
         }
     }
 }
+//dto data transformation object
