38cc7548aad9d9be0b493edbd6ce2a509d46759e
Işıl BARDAKCI <bardakci.isil@hotmail.com>
2022-07-29 12:35:46 +0300

    29.07.2022

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,4 +1,5 @@
 ﻿using Business.Abstract;
+using Business.Constants;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
@@ -23,38 +24,48 @@ namespace Business.Concrete
             
             if(product.ProductName.Length<2)
             {
-                return new ErrorResult("Urun ismi min 2 karakter olmalidir");
+                //magic strings: "dumduzstring"
+                return new ErrorResult(Messages.ProductNameInvalid);
             }
             _productDal.Add(product);
             // return new Result(true,"urun eklendi"); yalnızca bu satiri yazamayiz constructor eklememiz lzm
-            return new SuccessResult("urun eklendi");
+            return new SuccessResult(Messages.ProductAdded);
 
 
         }
 
-        public List<Product> GetAll()
+        public IDataResult<List<Product>> GetAll()
         {
-            return _productDal.GetAll();    
+            if(DateTime.Now.Hour==22)
+            {
+                return new ErrorDataResult<List<Product>>(Messages.MaintananceTime);
+            }
+            return new SuccessDataResult<List<Product>>(_productDal.GetAll(),Messages.ProductListed);    
         }
 
-        public List<Product> GetAllByCategoryId(int id)
+        public IDataResult<List<Product>> GetAllByCategoryId(int id)
         {
-            return _productDal.GetAll(p=>p.CategoryId == id);   
+            return new SuccessDataResult<List<Product>>(_productDal.GetAll(p=>p.CategoryId == id));   
         }
 
-        public List<Product> GetAllByUnitPrice(decimal min, decimal max)
+        public IDataResult<List<Product>> GetAllByUnitPrice(decimal min, decimal max)
         {
-            return _productDal.GetAll(p=>p.UnitPrice<=min && p.UnitPrice<=max);
+            return new SuccessDataResult<List<Product>>(_productDal.GetAll(p=>p.UnitPrice<=min && p.UnitPrice<=max));
         }
 
-        public Product GetById(int prodctId)
+        public IDataResult<Product> GetById(int prodctId)
         {
-            return _productDal.Get(p=>p.ProductId==prodctId);
+            return new SuccessDataResult<Product>(_productDal.Get(p=>p.ProductId==prodctId));
         }
 
-        public List<ProductDetailDto> GetProductDetails()
+        public IDataResult<List<ProductDetailDto>> GetProductDetails()
         {
-            return _productDal.GetProductDetails();
+            if(DateTime.Now.Hour == 12)
+            {
+                return new ErrorDataResult<List<ProductDetailDto>>(Messages.MaintananceTime);
+            }
+            return new SuccessDataResult<List<ProductDetailDto>>(_productDal.GetProductDetails());
         }
     }
 }
+
---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,16 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        public static string ProductAdded = "Urun Eklendi";
+        public static string ProductNameInvalid = "Urun ismi gecersiz";
+        internal static string MaintananceTime = "Sistem bakımda";
+        internal static string ProductListed = "Urunler listelendi";
+    }
+}
+//1.37.29
