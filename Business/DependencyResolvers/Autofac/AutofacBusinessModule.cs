3ba5dfb111d6b1febc1659181b8982ae5dcd4d02
Işıl BARDAKCI <bardakci.isil@hotmail.com>
2022-08-01 09:47:42 +0300

    week3 day1

---
+++ Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -0,0 +1,24 @@
+﻿using Autofac;
+using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.DependencyResolvers.Autofac
+{
+    public class AutofacBusinessModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<ProductManager>().As<IProductService>().SingleInstance();
+            builder.RegisterType<EFProductDal>().As<IProductDal>().SingleInstance();
+        }
+    }
+}
+
+//startupta 33 34 ün yaptigi isi yapar. autofacta kayit (instance gelistirmenin yolu ortami yukarısıdır.)
+//apidaki program csde yazilan kod: ios olarak kendi altyapını kllanma ben businesdeki autofac business modulu kullanarak yapı verim dersen o.o
+//webApi -> program.cs ( host.createDefaultBuilder(args) - .ConfigureWebHostDefaults() ) 
