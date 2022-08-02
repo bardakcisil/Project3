005908490b60136b640cf28f850c2ab78186c431
Işıl BARDAKCI <bardakci.isil@hotmail.com>
2022-08-02 11:48:29 +0300

    2.08.2022

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,4 +1,5 @@
 ﻿using Business.Abstract;
+using Business.CCS;
 using Business.Constants;
 using Business.ValidationRules.FluentValidation;
 using Core.Aspects.Autofac.Validation;
@@ -17,19 +18,27 @@ namespace Business.Concrete
     public class ProductManager : IProductService
     {
         IProductDal _productDal;
+        
         public ProductManager(IProductDal productDal)
         {
             _productDal = productDal;
+            
         }
 
 
         [ValidationAspect(typeof(ProductValidator))]
         public IResult Add(Product product)
-        {         
-            _productDal.Add(product);
-            // return new Result(true,"urun eklendi"); yalnızca bu satiri yazamayiz constructor eklememiz lzm
-            return new SuccessResult(Messages.ProductAdded);
+        {
+            //ayni isimde urun eklenemez
 
+             if (CheckIfProductCountofCategoryCorrect(product.CategoryId).Success)
+            { 
+             //busines code
+            _productDal.Add(product);
+             // return new Result(true,"urun eklendi"); yalnızca bu satiri yazamayiz constructor eklememiz lzm
+             return new SuccessResult(Messages.ProductAdded);
+            }
+            return new ErrorResult();
 
         }
 
@@ -64,6 +73,29 @@ namespace Business.Concrete
                 return new ErrorDataResult<List<ProductDetailDto>>(Messages.MaintananceTime);
             }*/
             return new SuccessDataResult<List<ProductDetailDto>>(_productDal.GetProductDetails());
+        }
+
+        [ValidationAspect(typeof(ProductValidator))]
+        public IResult Update(Product product)
+        {
+            //bir kategoride en fazla 10 urun olabilir
+            var result = _productDal.GetAll(p => p.CategoryId == product.CategoryId).Count;
+            if (result >= 10)
+            {
+                return new ErrorResult(Messages.ProductCountofCategoryError);
+            }
+            throw new NotImplementedException();
+        }
+
+        private IResult CheckIfProductCountofCategoryCorrect(int categoryId)
+        {
+            //select count(*) from products where categoryId=1
+            var result = _productDal.GetAll(p => p.CategoryId == categoryId).Count;
+            if (result >= 10)
+            {
+                return new ErrorResult(Messages.ProductCountofCategoryError);
+            }
+            return new SuccessResult();
         }
     }
 }
--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -9,8 +9,9 @@ namespace Business.Constants
     {
         public static string ProductAdded = "Urun Eklendi";
         public static string ProductNameInvalid = "Urun ismi gecersiz";
-        internal static string MaintananceTime = "Sistem bakımda";
-        internal static string ProductListed = "Urunler listelendi";
+        public static string MaintananceTime = "Sistem bakımda";
+        public static string ProductListed = "Urunler listelendi";
+        public static string ProductCountofCategoryError="1 kategoride en fazla 10 urun olabilir";
     }
 }
 //1.37.29
