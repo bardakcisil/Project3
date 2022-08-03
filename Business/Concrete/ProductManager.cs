0a915363657ea21810a7097760bbf7d3267193f9
Işıl BARDAKCI <bardakci.isil@hotmail.com>
2022-08-03 13:47:35 +0300

    03.08.2022 13:47

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,4 +1,5 @@
 ﻿using Business.Abstract;
+using Business.BusinessAspects.Autofac;
 using Business.CCS;
 using Business.Constants;
 using Business.ValidationRules.FluentValidation;
@@ -28,7 +29,7 @@ namespace Business.Concrete
             _categoryService = categoryService;   
         }
 
-        
+        [SecuredOperation("product.add,admin")]
         [ValidationAspect(typeof(ProductValidator))]
         public IResult Add(Product product)
         {
@@ -97,7 +98,7 @@ namespace Business.Concrete
         {
             //select count(*) from products where categoryId=1
             var result = _productDal.GetAll(p => p.CategoryId == categoryId).Count;
-            if (result >= 10)
+            if (result >= 15)
             {
                 return new ErrorResult(Messages.ProductCountofCategoryError);
             }
--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -1,6 +1,7 @@
 ﻿using Entities.Concrete;
 using System;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
 using System.Text;
 
 namespace Business.Constants
@@ -14,7 +15,15 @@ namespace Business.Constants
         public static string ProductCountofCategoryError="1 kategoride en fazla 10 urun olabilir";
         public static string ProductNameAlreadyExists="Bu isimde zaten başka bir ürün var";
 
+        public static string UserNotFound = "Kullanıcı bulunamadı";
+        public static string PasswordError = "Şifre hatalı";
+        public static string SuccessfulLogin = "Sisteme giriş başarılı";
+        public static string UserAlreadyExists = "Bu kullanıcı zaten mevcut";
+        public static string UserRegistered = "Kullanıcı başarıyla kaydedildi";
+        public static string AccessTokenCreated = "Access token başarıyla oluşturuldu";
+
         public static string CategoryLimitExceded = "Kategori Limiti Aşıldığı için Yeni ürün eklenemedi";
+        public static string AuthorizationDenied = "Yetkiniz yok.";
     }
 }
 //1.37.29
